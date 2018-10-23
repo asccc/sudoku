bb8bb5d712c3b65d89334ba8951ce349766ad6f8
asccc <git@putc.de>
2018-10-23 10:26:58 +0200

    contest version

--- src/ssud.c
+++ src/ssud.c
@@ -16,13 +16,10 @@
 #include <stdbool.h> /* bool, true false */
 #include <string.h> /* memcpy */
 #include <pthread.h> /* pthread ... */
-#include <assert.h> /* assert */
-#include <unistd.h> /* usleep */
 
 #define NOINDEX (9*9)+1
-#define SRUNNING 1
-#define SFAILURE 2
-#define SSUCCESS 3
+#define SFAILURE 0
+#define SSUCCESS 1
 
 /**
  * program options
@@ -61,7 +58,9 @@ static bool check_number (
   unsigned row, 
   unsigned col
 ) {
-  assert(grid != 0);
+  if (!grid) {
+    whops("NULL passed to %s", __func__);
+  }
   /* calculate region */
   const unsigned rx = row / 3 * 3;
   const unsigned ry = col / 3 * 3;
@@ -100,7 +99,6 @@ signed calc_score (
   unsigned row,
   unsigned col
 ) {
-  assert(grid != 0);
   unsigned seen[10] = {0};
   /* calculate region */
   const unsigned rx = row / 3 * 3;
@@ -130,25 +128,19 @@ signed calc_score (
  * @param  grid the sudoku grid
  * @return      the index or 81 if no index was found
  */
-static unsigned find_slot (
+unsigned find_best_index (
   unsigned grid[]
 ) {
-  assert(grid != 0);
   unsigned idx = NOINDEX;
   /* score benchmark, the higher the better */
   signed csc = 0;
   signed psc = -1;
-  for (unsigned i = 0; i < (9*9); ++i) {
-
-  }
+  unsigned row;
+  unsigned col;
   for (row = 0; row < 9; ++row) {
     for (col = 0; col < 9; ++col) {
       if (grid[row * 9 + col] == 0) {
         csc = calc_score(grid, row, col);
-        if (csc > 7) {
-          /* best possible result */
-          return row * 9 + col;
-        }
         if (csc > psc) {
           psc = csc;
           idx = row * 9 + col;
@@ -161,7 +153,7 @@ static unsigned find_slot (
 
 /**
  * tries to find a solution for the given puzzle.
- * nothing fancy, just a simple/stupid xxx (badword on github!)
+ * nothing fancy, just a simple/stupid brute-force
  *
  * single threaded
  *
@@ -172,18 +164,21 @@ static unsigned find_slot (
 static bool find_solution_st (
   unsigned grid[]
 ) {
-  assert(grid != 0);
+  if (!grid) {
+    whops("NULL passed to %s", __func__);
+  }
+
   /* find the first empty slot 
     with least possibilities */
   unsigned idx;
-  idx = find_slot(grid);
+  idx = find_best_index(grid);
 
   if (idx == NOINDEX) {
     /* no empty slot found */
     return true;
   }
 
-  /* slot is empty, start xxx (badword on github) */
+  /* slot is empty, start brute-force */
   const unsigned row = idx / 9;
   const unsigned col = idx % 9;
   /* this loop is manually unrolled */
@@ -192,7 +187,7 @@ static bool find_solution_st (
   #define UNROLLED_CHECK(num)                \
     if (check_number(grid, num, row, col)) { \
       grid[idx] = num;                       \
-      if (find_solution_st(grid)) {          \
+      if (find_solution_st(grid)) { \
         return true;                         \
       }                                      \
       grid[idx] = 0;                         \
@@ -218,20 +213,18 @@ static bool find_solution_st (
  */
 static void * find_solution_th (void *pass) 
 {
-  assert(pass != 0);
-  unsigned *smem = pass;
-  unsigned *stat = smem; /* status slot */
-  unsigned *grid = smem + 1;
-  /* set cancel state */
-  pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, 0);
+  unsigned *slot = pass;
+  unsigned *stat = slot; /* status slot */
+  unsigned *grid = slot + 1;
+  /* default status is FAILURE */
+  *stat = SFAILURE;
   /* use the single-thread solver */
   if (find_solution_st(grid)) {
     /* solution was found */
     *stat = SSUCCESS;
-  } else {
-    *stat = SFAILURE;
   }
-  /* get out */
+  /* pass everything back */
+  pthread_exit(pass);
   return 0;
 }
 
@@ -240,86 +233,62 @@ static void * find_solution_th (void *pass)
  *
  * @param pt   the thread handle
  * @param grid the grid memory for this thread
- * @param smem the memory slot for this thread
+ * @param slot the memory slot for this thread
  * @param idx  the index in the grid we're at
  * @param num  the number to be tested
  * @return     the memory slot
  */
-static void solve_fork (
+static void solv_fork (
   pthread_t *const pt,
   unsigned grid[],
-  unsigned *smem,
+  unsigned *slot,
   unsigned idx,
   unsigned num
 ) {
-  assert(grid != 0);
-  assert(smem != 0);
   /* first slot is for the success/failure status */
-  memcpy(smem + 1, grid, sizeof(unsigned)*9*9);
+  slot[0] = SFAILURE;
+  memcpy(slot + 1, grid, sizeof(unsigned)*9*9);
+
   /* fill in the number to test */
-  smem[0] = SRUNNING;
-  smem[idx + 1] = num;
+  slot[idx + 1] = num;
+
   /* fork off! */
-  pthread_create(pt, 0, find_solution_th, smem);
+  pthread_create(pt, 0, find_solution_th, slot);
 }
 
 /**
  * joins the given thread back
  *
  * @param  pt   the pthread handle
- * @param  pi   thread id
  * @param  grid the grid memory
- * @param  copy true if the result should be copied
- * @param  tmem  the thread memory
+ * @param  copy true if the result should be copied 
  * @return      true if a solution was found
  */
-static bool solve_join (
+static bool solv_join (
   pthread_t *const pt,
-  const unsigned pi,
   unsigned grid[],
-  bool *const sfnd,
-  const unsigned *const tmem
+  const bool copy
 ) {
-  assert(pt != 0);
-  assert(grid != 0);
-  assert(tmem != 0);
-
-  /* unsigned int should be atomic, but just to be safe */
-  static pthread_mutex_t mtx =
-    PTHREAD_MUTEX_INITIALIZER;
-
-  if (*sfnd) {
-    /* thread no longer needed */
-    pthread_cancel(*pt);
-    pthread_join(*pt, 0);
-    return true;
-  }
-
-  /* check thread status */
-  pthread_mutex_lock(&mtx);
-
-  if (*tmem == SRUNNING) {
-    /* thread is still running */
-    pthread_mutex_unlock(&mtx);
+  unsigned *slot;
+  if (copy) {
+    /* join thread */
+    pthread_join(*pt, (void**)&slot);
+    if (*slot == SSUCCESS) {
+      /* copy solution */
+      memcpy(grid, slot + 1, sizeof(unsigned)*9*9);
+      return true;
+    }
+    /* no solution */
     return false;
   }
-
+  /* cancel thread, result no longer needed */
+  pthread_cancel(*pt);
   pthread_join(*pt, 0);
-  
-  if (!*sfnd) {
-    if (*tmem == SSUCCESS) {
-      /* copy solution */
-      memcpy(grid, tmem + 1, sizeof(unsigned)*9*9);
-      *sfnd = true;
-    }
-  }
-
-  pthread_mutex_unlock(&mtx);
-  return true;
+  return false;
 }
 
 /**
- * starts the xxx (bad word on github) in n-threads
+ * starts the brute-force in n-threads
  *
  * @see find_solution_st
  *
@@ -330,22 +299,19 @@ static bool solve_join (
 static bool find_solution_mt (
   unsigned grid[]
 ) {
-  assert(grid != 0);
   /* keep things simple, stupid */
   /* one thread for each possible number */
   pthread_t pool[9] = {0};
   unsigned *smem[9] = {0};
   unsigned pidx = 0;
 
-  /* to keep track of running threads */
-  bool pact[9] = {false};
   /* status flag */
   bool sfnd = false;
 
   /* find the first empty slot 
     with least possibilities */
   unsigned idx;
-  idx = find_slot(grid);
+  idx = find_best_index(grid);
 
   if (idx == NOINDEX) {
     /* no empty slot found */
@@ -355,45 +321,33 @@ static bool find_solution_mt (
   /* we found a empty slot */
   const unsigned row = idx / 9;
   const unsigned col = idx % 9;
-
   /* start one thread for each possible number */
   for (unsigned num = 1; num <= 9; ++num) {
     if (check_number(grid, num, row, col)) {
-      unsigned *tmem = calloc(9*9+1, sizeof(unsigned));
       pthread_t *thrd = &pool[pidx];
-      solve_fork(thrd, grid, tmem, idx, num);
+      /* in order to keep valgrind happy ... 
+        we use calloc instead of malloc */
+      smem[pidx] = calloc(9*9+1, sizeof(unsigned));
+      if (!smem[pidx]) {
+        whops("out of memory in %s", __func__);
+      }
+      /* start thread */
+      solv_fork(thrd, grid, smem[pidx], idx, num);
       /* next thread */
-      pact[pidx] = true;
-      smem[pidx] = tmem;
       pidx += 1;
     }
   }
-
-  /* how many threads are in use */
-  unsigned puse = pidx;
-
-  /* wait for all threads to come back */
-  while (puse > 0) {
-    /* join threads and check if one came 
-      back with a solution */
-    for (unsigned pi = 0; pi < pidx; ++pi) {
-      if (pact[pi] == false) {
-        continue;
-      }
-      /* handle thread */
-      pthread_t *thrd = &pool[pi];
-      unsigned *tmem = smem[pi];
-      if (solve_join(thrd, pi, grid, &sfnd, tmem)) {
-        /* thread came back */
-        puse -= 1;
-        pact[pi] = false;
-        free(tmem);
-      }
-      /* wait a bit */
-      usleep(10);
+  /* join threads and check if one came 
+    back with a solution */
+  for (unsigned pi = 0; pi < pidx; ++pi) {
+    pthread_t *thrd = &pool[pi];
+    if (solv_join(thrd, grid, !sfnd)) {
+      /* solution found! */
+      sfnd = true;
     }
+    /* cleanup memory */
+    free(smem[pi]);
   }
-
   /* stop here */
   return sfnd;
 }
@@ -409,8 +363,10 @@ static inline bool solve_puzzle (
   unsigned grid[],
   bool use_threads
 ) {
-  assert(grid != 0);
-  /* start xxx (badword on github) */
+  if (!grid) {
+    whops("NULL passed to %s", __func__);
+  }
+  /* start brute-force */
   if (use_threads) {
     /* multi-threaded */
     return find_solution_mt(grid);
@@ -429,8 +385,6 @@ static void print_puzzle_fancy (
   unsigned grid[],
   FILE *out
 ) {
-  assert(grid != 0);
-  assert(out != 0);
   #if defined(_WIN32)
     /* windows does not like UTF8 stuff in the console */
     /* UTF8 is (somewhat) supported, but ... linux is superior! */
@@ -462,7 +416,7 @@ static void print_puzzle_fancy (
         fprintf(out, "%s %u ", col % 3 ? "│" : "┃", grid[idx]);
       }
       if (col++ > 7) {
-        fputs("┃", out);
+        fputs("┃", stdout);
         if (row++ < 8) {
           if (row % 3) {
             fputs("\n┠───┼───┼───╂───┼───┼───╂───┼───┼───┨\n", out);
@@ -492,13 +446,19 @@ static void print_puzzle (
   FILE *out,
   bool fancy
 ) {
-  assert(grid != 0);
-  assert(out != 0);
+  if (!grid) {
+    whops("NULL passed to %s", __func__);
+  }
 
   if (!fancy) {
     /* simple output format used by @German */
+    unsigned col = 0;
     for (unsigned idx = 0; idx < (9*9); ++idx) {
-      fprintf(out, "%u", grid[idx]);
+      fprintf(stdout, "%u", grid[idx]);
+      if (col++ > 7) {
+        fputs("\n", out);
+        col = 0;
+      }
     }
   } else {
     print_puzzle_fancy(grid, out);
@@ -517,8 +477,6 @@ static void read_puzzle_input (
   unsigned grid[],
   FILE *inp
 ) {
-  assert(grid != 0);
-  assert(inp != 0);
   /* for error reporting */
   unsigned rows[9][9] = {{0}};
   unsigned cols[9][9] = {{0}};
@@ -600,12 +558,11 @@ static void read_puzzle_input (
  * @param argc program argument count
  * @param argv program argument values
  */
-static void parse_sopts (
+void parse_sopts (
   struct sopts *opts,
   int argc,
   char *argv[]
 ) {
-  assert(opts != 0);
   opts->threads = true;
   opts->fancyof = false;
   opts->help = false;
